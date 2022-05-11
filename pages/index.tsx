45d37e757f3f600749fe70ac566214c2624b45ef
Fabrizio Onorio <onoriofabrizio@gmail.com>
2022-05-11 16:29:18 +0200

    added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,31 @@ import { Projects } from "../components/Projects";
 import { Contacts } from "../components/Contacts";
 import { Nav } from "../components/Nav";
 import { Footer } from "../components/Footer";
+import Head from "next/head";
 
 const Home: NextPage = () => {
 	return (
 		<section>
+			<Head>
+				<title>Fabrizio Onorio - Javascript Web Developer</title>
+				<meta
+					name="description"
+					content="Fabrizio Onorio - Javascript Web Developer"
+				/>
+				<meta property="og:type" content="website" />
+				<meta
+					name="og:title"
+					property="og:title"
+					content="Fabrizio Onorio - Javascript Web Developer"
+				/>
+				<meta
+					name="og:description"
+					property="og:description"
+					content="Fabrizio Onorio - Javascript Web Developer"
+				/>
+				<link rel="icon" type="image/png" href="/static/images/favicon.ico" />
+				<link rel="apple-touch-icon" href="/static/images/favicon.ico" />
+			</Head>
 			<Nav />
 			<section className="main-page" id={"home"}>
 				<Title />
