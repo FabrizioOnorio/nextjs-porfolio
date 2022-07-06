f9625842bedf61a327f3c52949642ad8ea645572
Fabrizio Onorio <onoriofabrizio@gmail.com>
2022-07-06 11:18:01 +0200

    marketplace

--- pages/rails-marketplace.tsx
+++ pages/rails-marketplace.tsx
@@ -8,35 +8,25 @@ const RailsMarketPlacePage: NextPage = () => {
 		<>
 			<Background />
 			<section className="project-page-section">
-				<h1>Guess Who Game</h1>
+				<h1>Market Place for Fabrics</h1>
 				<section className="project-page-description">
 					<Image
 						objectFit="cover"
 						width={350}
 						height={350}
-						src={"/guess-who.png"}
-						alt={"prototype screenshots of the recipes project"}
+						src={"/fabrics.png"}
+						alt={"prototype screenshots of the fabrics airbnb clone project"}
 						className="project-page-img"
 					/>
 					<section className="project-page-text">
 						<p>
-							This application is the implementation of the famous{" "}
-							<Link href="https://en.wikipedia.org/wiki/Guess_Who%3F">
-								<a target="_blank">Guess Who</a>
-							</Link>{" "}
-							game.<br></br> The app was made using React with Typescript and
-							Redux for the front-end, for the back-end we used Express together
-							with a socket.io.
+							A Ruby on Rails Desktop-Web-App. This was our first team project
+							in Le Wagon Coding Bootcamp. The use if for selling recycled
+							fabrics.
 						</p>
 						<section className="projects-page-links">
-							<Link href="https://guess-who-salt-game.herokuapp.com/">
-								<a target="_blank">Visit the heroku page</a>
-							</Link>
-							<Link href="https://github.com/FabrizioOnorio/guess-who">
-								<a target="_blank">Code on GitHub for the main app</a>
-							</Link>
-							<Link href="https://github.com/FabrizioOnorio/guess-who-socket">
-								<a target="_blank">Code on GitHub for the socket server</a>
+							<Link href="https://github.com/Sarowa158/fabrics_marketplace">
+								<a target="_blank">Code on GitHub</a>
 							</Link>
 						</section>
 					</section>
