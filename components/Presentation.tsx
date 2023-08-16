e6b332a32271257e2ad5cd11518b81e63b4d634f
Fabrizio <onoriofabrizio@gmail.com>
2023-08-16 08:50:30 +0200

    landing page

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -4,11 +4,11 @@ export const Presentation: React.FC = () => {
 	return (
 		<section className="w-full h-screen flex flex-col items-center justify-center" id={"home"}>
 				<div className="text-xl font-semibold flex flex-col items-center justify-center">
-					<h1 className="text-3xl"><span className="text-orange-700 text-6xl">Hello!</span> I&apos;m Fabrizio,</h1>
+					<h1 className="text-3xl">Hello! I&apos;m Fabrizio,</h1>
 					<h3>a Full-Stack <span className="text-yellow-400">TypeScript</span> Developer</h3>
 				</div>
 			<section className="w-full flex justify-center">
-				<h4 className="text-lg mt-3 max-w-3/6 break-all ">
+				<h4 className="text-lg mt-4 mx-4 max-w-3/6 break-all ">
 					I love using code, for making products that people love using.
 				</h4>
 			</section>
